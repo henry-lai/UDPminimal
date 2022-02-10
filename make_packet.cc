7426e77842d714cf535f181d6d99374d0a7a1648
henry.lai1 <henry.lai@n-hop.com>
2022-02-10 18:01:28 +0800

    Added sendto()

--- make_packet.cc
+++ make_packet.cc
@@ -9,10 +9,11 @@ using namespace std;
 int main()
 {
     // allocate memory for a packet
-    unsigned char* pkt = (unsigned char*)calloc(1,sizeof(MAX_PACKET_SIZE));
-
-    //allocating memory in pkt for ip header
+    unsigned char* pkt = (unsigned char*)calloc(1,sizeof(struct ip) +sizeof(MAX_PACKET_SIZE));
     struct ip *header = (struct ip*)pkt;
+    unsigned char* data = pkt+sizeof(struct  ip);
+    
+
     header->ip_v = 4;
     header->ip_hl = 5;
     header->ip_id = htons(0);
@@ -20,9 +21,24 @@ int main()
     header->ip_ttl = 64;
     header->ip_p = 17; // udp protocol
     
+    //mke socket buffer to store data
+    for (int i = 0; i < MAX_PACKET_SIZE; i++)
+    {
+        data[i] = i%256;
+    }
+    
+   
+
     sockaddr_in destAddr;
     destAddr.sin_family = AF_INET;
 
+    //make a udp socket file descriptor
+    int32_t socfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    if(socfd < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    sendto(socfd, data, (size_t)(sizeof(struct ip)+pkt), 0, (struct sockaddr * )&destAddr, sizeof(destAddr));
     
     cout << pkt << endl;
 }
