f34333b78de4041b865a984ce0968f3969295426
henry.lai1 <henry.lai@n-hop.com>
2022-02-11 14:26:11 +0800

    Fixed packet allocate, no loger segfaults

--- make_packet.cc
+++ make_packet.cc
@@ -1,44 +1,49 @@
 #include <netinet/ip.h>
 #include <stdio.h>
 #include <iostream>
-#include "config.hpp"
+//#include "config.hpp"
 
 using namespace std;
-
+#define MAX_FILE_SIZE 65535
 
 int main()
 {
-    // allocate memory for a packet
-    unsigned char* pkt = (unsigned char*)calloc(1,sizeof(struct ip) +sizeof(MAX_PACKET_SIZE));
-    struct ip *header = (struct ip*)pkt;
-    unsigned char* data = pkt+sizeof(struct  ip);
+    //allocate memory for single packet 
+    unsigned char* pkt = (unsigned char*) calloc( 1, sizeof(struct ip*) +  MAX_FILE_SIZE);
     
-
-    header->ip_v = 4;
-    header->ip_hl = 5;
-    header->ip_id = htons(0);
-    header->ip_off = htons(0);
-    header->ip_ttl = 64;
-    header->ip_p = 17; // udp protocol
+    // allocate memory for header                                
+    // struct ip *header = (struct ip *)pkt;
+    // header->ip_v = 4;
+    // header->ip_hl = 4;
+    // header->ip_id = htons(0);
+    // header->ip_off = htons(0);
+    // header->ip_ttl = 8;
+    // header->ip_p = 8; 
     
-    //mke socket buffer to store data
-    for (int i = 0; i < MAX_PACKET_SIZE; i++)
+    unsigned char* data = pkt + sizeof(struct  ip*);
+    
+    //unsigned char* data = (unsigned char *)sizeof(MAX_FILE_SIZE );
+    
+    //put bytes into packet
+    for (int i = 0; i < MAX_FILE_SIZE; i++)
     {
         data[i] = i%256;
     }
-    
-   
+
+    //data[0] = sizeof(1);
 
     sockaddr_in destAddr;
     destAddr.sin_family = AF_INET;
+    
 
-    //make a udp socket file descriptor
-    int32_t socfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    //make a  socket file descriptor
+    int32_t socfd = socket(AF_INET, SOCK_RAW, 0);
     if(socfd < 0)
     {
         exit(EXIT_FAILURE);
     }
+    
     sendto(socfd, data, (size_t)(sizeof(struct ip)+pkt), 0, (struct sockaddr * )&destAddr, sizeof(destAddr));
     
-    cout << pkt << endl;
+    
 }
